7ce84339ee1c6fbfcc06fb96b5938e7e090bf3e7
skywalker-alt327 <itisgoingtobeavictory2.39@gmail.com>
2023-05-25 12:45:53 -0400

    added command line arguments options to select the interface to change its mac address

--- mac_changer.py
+++ mac_changer.py
@@ -5,8 +5,11 @@ parser = optparse.OptionParser()
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
 parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
 
-interface = input("Interface>")
-new_mac = input("New Mac>")
+values, args = parser.parse_args()
+
+interface = values.interface
+new_mac = values.new_mac
+
 print(f"[+] Changing current MAC address of {interface} to new address {new_mac}")  
 
 subprocess.call(["ifconfig", interface, "down"])
